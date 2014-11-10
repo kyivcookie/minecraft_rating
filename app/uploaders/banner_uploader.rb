95f92f2d69b9a5ae0c9000ca51d7aa84fabd61a6
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-11-10 10:37:35 -0500

    fix php version

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -13,7 +13,11 @@ class BannerUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/home/unrealm/webapps/minecraft_rating/shared/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    '/tmp/servers'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
