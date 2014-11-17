d98e85a6072e6a30da674cd5965bc92037c42d03
Andrey Pristupa <andrey.pristupa@gubagoo.com>
2014-11-17 07:31:09 -0500

    redesign servers

--- app/models/ping/servers_ping.rb
+++ app/models/ping/servers_ping.rb
@@ -1,7 +1,7 @@
 class ServersPing < ActiveRecord::Base
   establish_connection(
       :adapter  => 'mysql2',
-      :database => 'minecraft_rating',
+      :database => 'mincraft_rating',
       :username => 'unrealm',
       :host     => 'localhost',
       :password => 'hjujdyj',
