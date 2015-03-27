500ed7a5caec1e4cca1e7a935eddb5a2bc7aa5cf
Ali <alialhaulani@ukr.net>
2015-03-27 20:21:27 +0200

    init

--- sitemap.rb
+++ sitemap.rb
@@ -3,5 +3,5 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://minecraftservers-ip.com'
 SitemapGenerator::Sitemap.create do
-  add '/contact', :changefreq => 'daily', :priority => 0.9
+  add '/contacts', :changefreq => 'daily', :priority => 0.9
 end
